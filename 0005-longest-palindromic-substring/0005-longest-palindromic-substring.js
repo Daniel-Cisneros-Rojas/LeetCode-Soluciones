3f555bbd1716d68fb333aa98ab7fc8e0b7666d8c
Daniel Cisneros Rojas <60678815+Daniel-Cisneros-Rojas@users.noreply.github.com>
2026-09-01 11:48:42 -0600

    Time: 2487 ms (5.02%), Space: 55 MB (74.09%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    
+    let resultado=[];
+    for(let i=0;i<s.length;i++){
+        for(let j=s.length-1;j>i;j--){
+
+            let inicio=i;
+            let fin=j;
+            let valida=true;
+            while(valida&&inicio<fin){
+              if(s[inicio]==s[fin]){
+                inicio++;
+                fin--;
+              }else{
+                valida=false;
+              }
+            }
+            
+            if(valida&&(j-i)>=resultado.length){
+              resultado=s.substring(i,j+1);
+              
+            }
+            
+        }
+    }
+
+    return resultado.length>0? resultado: s[0];
+};
