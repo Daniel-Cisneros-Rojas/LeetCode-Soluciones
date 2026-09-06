ee197453e50993221e853d8df32c10ea053d56a2
Daniel Cisneros Rojas <60678815+Daniel-Cisneros-Rojas@users.noreply.github.com>
2026-09-06 11:27:40 -0600

    Time: 52 ms (5.3%), Space: 62.6 MB (5.22%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -0,0 +1,51 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function(s) {
+    let caracter_actual=0;
+    let positivo=true;
+    let numeros=[];
+    while(s[caracter_actual]==" "){
+        caracter_actual++;
+    }
+    if(s[caracter_actual]=="-"){
+        positivo=false;
+        caracter_actual++;
+
+    }else if(s[caracter_actual]=="+"){
+        positivo=true;
+        caracter_actual++;
+
+    }
+
+    
+    while(s[caracter_actual]=="0"){
+        caracter_actual++;
+    }
+    
+    for(let i=caracter_actual;i<s.length;i++){
+          
+          if(s.charCodeAt(i)>57||s.charCodeAt(i)<48){
+            break;
+          }else{
+            numeros.push(s.charCodeAt(i) - 48);
+          } 
+    }
+    
+    let resultado=0;
+    console.log(numeros);
+    console.log(2**31);
+    for(let i=0;i<numeros.length;i++){
+      resultado=resultado+numeros[numeros.length-1-i]*(10**i);
+    }
+    if(resultado>((2**31)-1)){
+        resultado= (2**31);
+        console.log("entro");
+        console.log(positivo);
+        resultado= positivo? resultado-1:resultado;
+    }
+    return positivo? resultado: resultado*-1;
+    
+    
+};
