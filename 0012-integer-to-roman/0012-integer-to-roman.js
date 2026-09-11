9c884fb3193472c2f3769812fc67de02c669fa99
Daniel Cisneros Rojas <60678815+Daniel-Cisneros-Rojas@users.noreply.github.com>
2026-09-11 11:51:38 -0600

    Time: 91 ms (5.04%), Space: 64.5 MB (7.11%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.js
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -4,15 +4,16 @@
  */
 var intToRoman = function(num) {
     let Symbolos=["M","D","C","L","X","V","I"];
+    //2 numeros grandes al final para evitar salir de rango al final
     let valores=[1000,500,100,50,10,5,1,1000,1000];
     let resultado="";
-    //verificar primero que no se pueda como resta con los dos siguiente snumeros y luego restarlo al total
 
     let i=1000;
     let j=0;
 
     while(num>0)
     {   
+        // se separa el numero en cifras para facilitar su manipulacion 
        let cifra = parseInt(num/i)*i;
        i=i/10;
        num=num-cifra;
@@ -23,44 +24,40 @@ var intToRoman = function(num) {
        
        while(cifra>0)
        {
+         //combinaciones restandole los simbolos siguientes (segun las reglas solo se puede con los 2 siguientes)
           let combinacion1=valores[j]-valores[j+1];
           let combinacion2=valores[j]-valores[j+2];
-          console.log("valor usado ", valores[j]);
           switch(true)
-          {
+          { 
+            //se verifica primero que no sea igual que el siguiente evitando combinaciones como LC=50 en lugar de el correcto L
             case (cifra==valores[j+1]):
-            console.log("caso 0");
-            console.log("cifra", cifra," - ", valores[j+1]);
+            
             cifra=cifra-valores[j+1];
             resultado=resultado + (Symbolos[j+1]);
             break;
 
             case (combinacion1==cifra):
-            console.log("caso 1");
-            console.log("cifra", cifra," - ", combinacion1);
+        
             cifra=cifra-combinacion1;
             resultado=resultado + (Symbolos[j+1]);
             resultado=resultado + (Symbolos[j]);
             break;
 
             case (combinacion2==cifra):
-            console.log("caso 2");
-            console.log("cifra", cifra," - ", combinacion1);
+           
             cifra=cifra-combinacion2;
             resultado=resultado + (Symbolos[j+2]);
             resultado=resultado + (Symbolos[j]);
             break;
-
+            
+            //no es necesario un contador a 3 por que siempre se puede representar de otra manera antes de sumar 4 numeros iguales
             case (cifra>=valores[j]):
-            console.log("caso 3");
-            console.log("cifra", cifra," - ", valores[j]);
             cifra=cifra-valores[j];
             resultado=resultado + (Symbolos[j]);
             break;
 
             case (cifra<valores[j]):
-            console.log("caso 4");
-            console.log("cifra siguiente");
+            //se continua con el siguiente valor
             j++;
             break;
 
